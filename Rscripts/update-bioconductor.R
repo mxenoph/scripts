3ad124e9f5dc765ff78ebd359f7d212e559e7cd9
Maria Xenophontos <mx222@cam.ac.uk>
2014-08-14 16:58:18 +0100

    moving R scripts to dir Rscript

--- Rscripts/update-bioconductor.R
+++
@@ -1,6 +0,0 @@
-bioc-updates <- function(url){
-    bioc= available.packages(contrib.url(url))
-    inst-pkgs= installed.packages()
-    common-pkgs= intersect(rownames(bioc), rownames(inst-pkgs))
-    mapply(function(x, y) compareVersion(x, y), inst-pkgs[common-pkgs, "Version"], bioc[common-pkgs, "Version"])
-}
