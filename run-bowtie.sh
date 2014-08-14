fb67eb4b71b39665f036f92d8daef5e1ed28530b
Maria Xenophontos <mx222@cam.ac.uk>
2014-08-14 15:26:07 +0100

    adding wrapper for bam-to-bw-strand-specific.sh

---
+++ run-bowtie.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Example usage: bsub -n 4 -M 20000 RunMapper.sh fastq genome mapper
+# e.g. genome = /nfs/research2/bertone/common/genome/MM9/MM9
+# Check if arguments are supplied
+if [ $# -eq 0 ]
+then
+    echo $0 'fastq genome mapper'
+fi
+
+# readlink converts relative to absolute path names
+fastq=$(readlink -m $1)
+genome=$(readlink -m $2)
+
+# Check if an argument was supplied for the mapper to use
+# -z checks if the argument is a NULL string and if so executed the body
+if [ -z "$3" ]
+then
+    # Set bowtie as the default mapper if an argument is not provided
+    mapper='bowtie'
+else
+    mapper=$3
+fi
+
+out=$PWD'/'$mapper'/'
+
+if [ ! -d $out ]
+then
+     mkdir $out
+     echo -e "Creating $out"
+fi
+
+if [ $mapper == 'bowtie' ]
+then
+    describer=$(sed 's/\(.fastq\|.txt\)$//' <<< $fastq | sed 's/.*\///')
+
+    # TODO: add more options if ever get PE reads for ChIP-seq or if I align RNA-Seq 
+    # with bowtie instead of gsnap (not gapped alignment)
+    # -p <n> launches parallel search threads => Always bsub it with -n 4
+    bowtie -m 1 -S -p 4 $genome $fastq > ${out}${describer}.sam
+    # Keeping a record of how the files in the directory where generated
+    echo -e `date +"%D%t%T"` "\t" "bowtie -m 1 -S $genome $fastq > ${out}${describer}.sam" >> ${out}FilesTree.log
+
+# TODO: add more peak callers if ever need to use a different one
+fi
+
+# SAM to BAM
+samtools view -bS ${describer}.sam -o ${describer}.uns.bam
+echo -e `date +"%D%t%T"` "\t" "samtools view -bS ${out}${describer}.sam -o ${out}${describer}.uns.bam" >> ${out}FilesTree.log
+
+# Sort BAM
+samtools sort ${describer}.uns.bam ${describer}.sort
+echo -e `date +"%D%t%T"` "\t" "samtools sort ${out}${describer}.uns.bam ${out}${describer}.sort" >> ${out}FilesTree.log
+
+# Remove intermediate files
+rm ${out}${describer}.uns.bam
--- run-gsnap.sh
+++ run-gsnap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Parse arguments#{{{
-ARGS=$(getopt -o f:g: -l "fastq:,genome:" -n "run-gsnap.sh" -- "$@")
+ARGS=$(getopt -o f:g:x:p: -l "fastq:,genome:index:nparallel:" -n "run-gsnap.sh" -- "$@")
 
 # Bad arguments
 if [ $? -ne 0 ]
@@ -17,11 +17,28 @@ do
             fastq="$2"; shift 2 ;;
         -g | --genome)
             genome="$2"; shift 2 ;;
+        -p | --nparallel)
+            N_JOBS="$2"; shift 2 ;;
+        -x | --index)
+            INDEX="$2"; shift 2 ;;
     esac
 done
 #}}}
 
-# GSNAP Options:#{{{
+echo $fastq;
+exit 1;
+# Output and basenames#{{{
+target_dir="$(dirname "$fastq")"/gsnap
+target_base="$(basename "$fastq")"
+output="${target_dir}/${target_base/.fastq.gz/.$INDEX/}"#}}}
+
+
+# Genome directories and splice-sites paths#{{{
+genome_dir=/nfs/research2/bertone/common/genome
+splice_dir=/nfs/research2/bertone/user/mxenoph/genome_dir
+#}}}
+
+# GSNAP Options Explanation:#{{{
 # -m: number of mismatches (for 75bp set to 4 or 5);
 # -i: indel penalty (default=2)...<2 can lead to FP at the end of the read
 # -N: look for novel splicing
@@ -30,46 +47,34 @@ done
 # -Q: print nothing if >n paths
 # -O: relevant if more than one worker thread (-t) -> prints output in the same order as input#}}}
 
+# Setting GSNAP OPTIONS#{{{
 N_THREADS=10
 # kmer size to use in genome database -- if not specified highest available in the db is used
 #K_MER=15
-
-# For running with -q option
-N_JOBS=20
-MEM=25000
-mismatch=7
+MISMATCH=7
 PROTOCOL='sanger'
-SPLICE_SITES='/nfs/research2/bertone/user/mxenoph/genome_dir/M_musculus_10/MM10.maps/mm10_ensembl70_splicesites.iit'
-
 
 if [ "$genome" == 'MM9' ]
 then
-   SPLICE_SITES='/nfs/research2/bertone/user/mxenoph/genome_dir/M_musculus_9/MM9.maps/mm9_ensembl67_splicesites.iit'
+    SPLICE_SITES=${splice_dir}/M_musculus_9/MM9.maps/mm9_ensembl67_splicesites.iit
+elif [ "$genome" == 'MM10' ]
+then
+    SPLICE_SITES=${splice_dir}/M_musculus_10/MM10.maps/mm10_ensembl70_splicesites.iit
+else
+    echo 'No splice sites provided. Exiting.'
+    exit 1;
 fi
 
 
-#run gsnap on a list of files
-while read infile
-do
- echo -e "Running gsnap on $infile"
- name=$(echo "$infile"|awk -F'_' '{print $2 "_" $3}')
- jobName="gsnap.$name[1-$N_JOBS]"
+OPT="-q $INDEX/$N_JOBS -t $N_THREADS"
+OPT="$OPT -D ${genome_dir}/$genome -d $genome"
+#Option -B is batch mode to make it run faster| (-i) --indel-penalty by default is 2, what is the benefit of setting it to mismatches? --trim-mismatch-score=0 turns off trimming at ends and will give FP at the ends of reads
+OPT="$OPT -B 5 -m $MISMATCH -i 1"
+#Find novel splicing and limit the size of splice junctions, define distant-splice penalty (-E) for when the intron length exits the value of -w
+OPT="$OPT -N 1 -w 100000 -E 100 -s $SPLICE_SITES"
+#Number of paths to print and 'don't print failed alignments', name output based on the q option split up
+OPT="$OPT -n 10 -Q -O --nofails -A sam --split-output=$output --gunzip --quality-protocol=$PROTOCOL"#}}}
 
-for ((i=0; i<$N_JOBS; i++)); do
-   OPT="-q $i/$N_JOBS -t $N_THREADS"
-   OPT="$OPT -D /nfs/research2/bertone/common/genome/$genome -d $genome"
-   #Option -B is batch mode to make it run faster| (-i) --indel-penalty by default is 2, what is the benefit of setting it to mismatches? --trim-mismatch-score=0 turns off trimming at ends and will give FP at the ends of reads
-   OPT="$OPT -B 5 -m $mismatch -i 1"
-   #Find novel splicing and limit the size of splice junctions, define distant-splice penalty (-E) for when the intron length exits the value of -w
-   OPT="$OPT -N 1 -w 100000 -E 100 -s $SPLICE_SITES"
-   #Number of paths to print and 'don't print failed alignments', name output based on the q option split up
-   OPT="$OPT -n 10 -Q -O --nofails -A sam --split-output=$out_dir/$name.$i --gunzip --quality-protocol=$PROTOCOL"
+# Run gsnap
+gsnap $OPT $fastq
 
-   submit=$(bsub -q research-rh6 -n $N_THREADS -N -M $MEM -R "rusage[mem=$MEM]" -oo "$out_dir/gsnap_$name.$i.log" -J "$name.$i" $gsnap $OPT $infile)
-
-   #NOTE: the splicesites.iit is hard-coded in the command..be sure it's the one you want before you run it
-  #submit=$(bsub -n 6 -N -M 20000 -R "rusage[mem=10000]" -J "gsnap" -oo "$out_dir/gsnap_$name.err" gsnap -d "$genome" -t 4 -m 5 -i 1 -N 1 -w 100000 -E 100 -n 10 -Q -q $i/10 -O -A sam --split-output="$name" --gunzip --quality-protocol=sanger -s /nfs/research2/bertone/user/mxenoph/genome_dir/M_musculus_10/MM10.maps/mm10_ensembl70_splicesites.iit "$infile")
-  #submit=$(bsub -n 6 -N -M 20000 -R "rusage[mem=10000]" -J "gsnap" -oo "$out_dir/gsnap_$name.err" gsnap -d "$genome" -t 4 -m 5 -i 1 -N 1 -w 100000 -E 100 -n 10 -Q -q $i/10 -O -A sam --split-output="$name" --gunzip --quality-protocol=sanger -s /nfs/research2/bertone/user/mxenoph/genome_dir/M_musculus_9/MM9.maps/mm9_ensembl67_splicesites.iit "$infile")
-  echo -e "$submit"
-  done
-done < "$files"
