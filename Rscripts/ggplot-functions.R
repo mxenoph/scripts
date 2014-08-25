a153f557dab3604062732c9767fb009abe32a275
Maria Xenophontos <mx222@cam.ac.uk>
2014-08-25 10:24:47 +0100

    function for extracting ggplot legend

--- Rscripts/ggplot-functions.R
+++ Rscripts/ggplot-functions.R
@@ -4,3 +4,11 @@ gg_color_hue <- function(n) {
     hues = seq(15, 375, length=n+1)
     hcl(h=hues, l=65, c=100)[1:n]
 }# }}}
+
+# Get ggplot legend
+g_legend<-function(p){# {{{
+    tmp <- ggplotGrob(p)
+    leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+    legend <- tmp$grobs[[leg]]
+    return(legend)}
+}# }}}
