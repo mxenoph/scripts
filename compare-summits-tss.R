19c9d53e576e828f8e6b4631f4198f57a71c45b7
Maria Xenophontos <mx222@cam.ac.uk>
2015-01-12 14:53:11 +0000

    arranging to functions

--- compare-summits-tss.R
+++ compare-summits-tss.R
@@ -1,11 +1,29 @@
-modules::import("annotation-functions", attach = T)
-get_annotation('mm10')
+#!/usr/bin/env Rscript
 
-source("~/source/Rscripts/functions.R")
-source("~/source/Rscripts/granges-functions.R")
-
-x <- c('rtracklayer', 'GenomicRanges', 'parallel')
+# Parsing command line arguments and create output subdirectories# {{{
+x <- c('argparse', 'tools')
 lapply(x, suppressMessages(library), character.only=T)
+parser <-  ArgumentParser(description="Compare summits to TSS")
+parser$add_argument('-p', '--path', metavar= "file", required='True', type= "character", help= "Path to look for MACS generated excel file for called peaks")
+parser$add_argument('-a', '--assembly', type= "character", default='mm9', help= "Give preferred assembly e.g. mm9. Default: mm9")
+parser$add_argument('-o', '--out', metavar= "path", type= "character", default= getwd(), help= "Output directory -- all subdirectories will be created here")
+parser$add_argument('-r', '--replotting', type= "character",  help= "To make the plots from an Rdata object")
+
+args <- parser$parse_args()
+
+output_path <- file.path(args$out, 'processed')
+plot_path <- file.path(args$out, 'plots')
+dir.create(output_path)
+dir.create(plot_path)
+
+# Load this before the other packages so that the import function from rtracklayer 
+# is not masked
+modules::import("annotation-functions", attach = T)
+get_annotation(args$assembly)
+
+source("~/source/Rscripts/granges-functions.R")
+x <- c('rtracklayer', 'GenomicRanges', 'parallel')
+lapply(x, suppressMessages(library), character.only=T)# }}}
 
 # Functions
 # x is the feature we want stuff centered to
@@ -23,73 +41,74 @@ get_position <- function(x, y){ # {{{
 }
 # }}}
 
-setwd("/nfs/research2/bertone/user/mxenoph/hendrich/chip/hendrich_2013/mm9/macs")
-peaks <- lapply(list.files(pattern=".*[^e]_peaks.xls"), macs2GRanges)
-names(peaks) <- list.files(pattern=".*[^e]_peaks.xls")
-# ranges of width 2Kb with center being the TSS
-tss_window <- import("/nfs/research2/bertone/user/mxenoph/genome_dir/M_musculus_10/MM10.maps/Mus_musculus.GRCm38.70_conv.tss_nocontig.gtf")
-tss <- GRanges(seqnames(tss_window),
-               IRanges(start = start(tss_window) + (width(tss)/2) -1, 
-                       width = 1),
-               strand = strand(tss_window))
+plot_scatter <- function(positions) {
+    for(x in 1:length(positions)) {
+        df <- positions[[x]]
+        p <- ggplot(df, aes(x= summit, y= fe)) + geom_point(col="grey50")
+        p <- p + scale_x_continuous(breaks=seq(-1000, 1000, by=200))
+        p <- p + geom_rug()
+        p <- p + ggtitle(names(positions)[x])
+        p <- p + annotate("text", y= max(df$fe), x =1000,
+                       # paste0 will not work cause it's not a plotmath function
+                       label= paste("n[peaks_in_window]", "==", nrow(df)),
+                       parse = TRUE,
+                       col = "red",
+                       hjust=1)
+        print(p)
+    }
+}
 
-chd4_wt <- GRanges(seqnames(peaks[[1]]),
-                   IRanges(start=values(peaks[[1]])[['summit']], end=values(peaks[[1]])[['summit']]),
-                   strand="*", 
-                   fe = values(peaks[[1]])[['FE']])
-ov <- findOverlaps(tss_window, chd4_wt)
+#setwd("/nfs/research2/bertone/user/mxenoph/hendrich/chip/hendrich_2013/mm9/macs")
 
-positions <- mclapply(1:length(ov), function(x, query, subj){# {{{
-                      pair <- ov[x]
-                      p <- get_position(query[queryHits(pair)], subj[subjectHits(pair)])
-                      d <- data.frame(summit = p, fe = values(subj[subjectHits(pair)])[['fe']])
-                      return(d)
-               }, tss, chd4_wt)
-positions <- do.call(rbind, positions)# }}}
+main <- function(){ #{{{
+    # Preparing the data
+    # Select the _peaks.xls files in the path and not the negative_peaks.xls
+    peaks <- lapply(list.files(pattern=".*[^e]_peaks.xls"), macs2GRanges)
+    names(peaks) <- list.files(pattern=".*[^e]_peaks.xls")
 
+    summits <- mclapply(peaks, function(gr){# {{{
+             gr <- GRanges(seqnames(gr),
+                       IRanges(start = values(gr)[['summit']], end = values(gr)[['summit']]),
+                       strand = "*",
+                       fe = values(gr)[['FE']])
+    })# }}}
 
-pdf('test_bins.pdf')
+    # tss and tss_window objects loaded from get_annotation(args$assembly) call
 
-ggplot(data=signal$sums, aes(x=range, y=value)) + geom_point() + scale_x_continuous(breaks=seq(-1000, 1000, by=200))
-dev.off()
+    # ranges of width 2Kb with center being the TSS
+    #tss_window <- import("/nfs/research3/bertone/user/mxenoph/genome_dir/M_musculus_10/MM10.maps/Mus_musculus.GRCm38.70_conv.tss_nocontig.gtf")
+    #tss <- GRanges(seqnames(tss_window),
+    #               IRanges(start = start(tss_window) + (width(tss_window)/2) -1,
+    #                       width = 1),
+    #               strand = strand(tss_window))# }}}
 
+    # Calculate position of summit relative to tss and keep fe for peak# {{{
+    positions <- mclapply(summits, function(summit) {
+             ov <- findOverlaps(tss_window, summit)
 
-positions <- mclapply(1:100, function(x, query, subj, centers){ # {{{
-                      ov <- findOverlaps(query[x], subj)
+             positions <- mclapply(1:length(ov), function(x, query, subj){
+                                   pair <- ov[x]
+                                   p <- get_position(query[queryHits(pair)], subj[subjectHits(pair)])
+                                   d <- data.frame(summit = p, fe = values(subj[subjectHits(pair)])[['fe']])
+                                   return(d)
+                           }, tss, summit)
 
-                     if ( length(queryHits(ov)) != 0 ){
-                         # If more than 1 hits in subj then I get a GRanges object
-                         # thus the function returns a vector of positions -- works fine
-                         p <- get_position(centers[x], subj[subjectHits(ov)])
-                         p <- p + 1000
-#                         names(p) <- values(query[x])[['gene_id']]
-#                         print(paste0('Position ', p, "  x=", x))
+             positions <- do.call(rbind, positions)
+             
 
-#                         sequence <- matrix(0, ncol=2000)
-#                         sequence[1, p] <- 1
-#                         rownames(sequence) <- values(query[x])[['gene_id']]
-                     } else {
-#                         sequence <- matrix(0, ncol=2000)
-#                         rownames(sequence) <- values(query[x])[['gene_id']]
-                         p <- 0
-                     }
-                     #return(sequence)
-                     return(p)
-}, tss_window, chd4_wt, tss)
+             return(positions)
+    })# }}}
 
-#positions <- as.data.frame(do.call(rbind, positions))
-#colnames(positions) <- 1:2000
-# }}}
-
-summarize_signal <- function(positions){# {{{
-    x <- c('reshape')
-    lapply(x, suppressMessages(library), character.only=T)
-
-    sums <- colSums(positions)
-    sums <- melt(sums)
-    sums$range <- as.numeric(rownames(sums)) - 1000
-
-    return(list(sums = sums))
 }
 # }}}
-signal <- summarize_signal(positions)
+
+
+# Printing
+if( !file.exists(file.path(output_path, 'peak-summitsVsTSS.Rda')) ) {
+    main()
+} else {
+    if ()
+}
+
+pdf('peak-summitsVsTSS.Rda')
+dev.off()
