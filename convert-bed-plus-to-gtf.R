3723a10a4620272c7af483db7a8693707f538a01
Maria Xenophontos <mx222@cam.ac.uk>
2015-01-29 16:37:11 +0000

    structured into functions

--- convert-bed-plus-to-gtf.R
+++ convert-bed-plus-to-gtf.R
@@ -12,31 +12,45 @@ args <- parser$parse_args()
 
 # TODO: read file extension, check column number and whether have the right format and convert all plus bed files to gtf
 
-x <- c('GenomicRanges', 'rtracklayer')
+x <- c('GenomicRanges', 'rtracklayer', 'plyr', 'tools')
 lapply(x, suppressMessages(library), character.only=T)
 
-bed <- read.table(args$bed, sep="\t")
-colnames(bed) <- c('chrom', 'chromStart', 'chromEnd', 'name', 'score', 'strand', 'signalValue', 'pValue', 'qValue', 'peak')[1:ncol(bed)]
-levels(bed$strand) <- gsub('.', "*", levels(bed$strand))
-bed$chromStart <- bed$chromStart - 1
+parse_broadPeak <- function(broad){# {{{
+    bed <- read.table(broad, sep="\t")
+    colnames(bed) <- c('chrom', 'chromStart', 'chromEnd',
+                       'name', 'score', 'strand',
+                       'signalValue', 'pValue', 'qValue', 'peak')[1:ncol(bed)]
+    
+    # Change levels so that strand matches -. + and * for GRanges instead of -1, +1, .
+    bed[['strand']] <- revalue(bed[['strand']], c("-1"="-", "+1" = "+", "." = "*"))
+    bed[['chromStart']] <- bed[['chromStart']] - 1
+    
+    grange <- with(bed, GRanges(chrom,
+                             IRanges(chromStart, chromEnd),
+                             strand,
+                             peak = name,
+                             score = score,
+                             fe = signalValue,
+                             pValue = pValue,
+                             qValue = qValue
+                             ))
 
-bed <- with(bed, GRanges(chrom,
-                         IRanges(chromStart, chromEnd),
-                         strand,
-                         peak = name,
-                         fe = signalValue,
-                         pValue = pValue,
-                         qValue = qValue
-                         ))
+    if (ncol(bed) == 10) values(grange)[['summit']] <- with(bed, 'peak')
+    
+    gtf <- paste0(gsub(file_ext(broad), '', broad), 'gtf')
+    export.gff(grange, gtf)
 
+    centered <- resize(grange,
+                       ifelse(width(grange) < median(width(grange)), width(grange), median(width(grange)) ),
+                       fix="center")
 
+    gtf <- paste0(gsub(file_ext(broad), '', broad), 'centered-median-window.gtf')
+    export.gff(centered, gtf)
+}# }}}
 
-gtf <- paste0(gsub(file_ext(args$bed), '', args$bed), 'gtf')
-export.gff(bed, gtf)
+if ( file_ext(args$bed) %in% c('broadPeak', 'narrowPeak')) {
+    parse_broadPeak(args$bed)
+} else {
+    warning("Can not convert bed file to gtf [unknown format].")
+}
 
-centered <- resize(bed,
-                   ifelse(width(bed) < median(width(bed)), width(bed), median(width(bed)) ),
-                   fix="center")
-
-gtf <- paste0(gsub(file_ext(args$bed), '', args$bed), 'centered-median-window.gtf')
-export.gff(bed, gtf)
