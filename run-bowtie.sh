c4216010f4f546e0e5d9ea8376c4c82fd3928b43
Maria Xenophontos <m.xenophontos.cy@gmail.com>
2015-05-18 23:28:40 +0100

    getopts added, and gzip fastq

--- run-bowtie.sh
+++ run-bowtie.sh
@@ -1,68 +1,85 @@
 #!/bin/bash
 
-# Example usage: bsub -n 4 -M 20000 run-bowtie.sh fastq genome_index mapper
-# e.g. genome = /nfs/research2/bertone/common/genome/MM9/MM9 
-# Check if arguments are supplied
-if [ $# -eq 0 ]
+#run-bowtie.sh #{{{
+usage() { echo "Usage: $0 [-f <fastq>] [-g <genome index directory>] [-o <output directory>]" 1>&2; exit 1; }
+
+# : means takes an argument but not mandatory (if mandatory will have to check after)
+options=':f:g:o:h'
+while getopts $options option
+do
+    case $option in
+        f ) fastq=(${OPTARG}) ;;
+        g ) GENOME=(${OPTARG}) ;;
+        o ) OUT=${OPTARG} ;;
+        h ) usage ;;
+        : ) echo "Missing option argument for -$OPTARG" >&2; usage ;;
+        \?) echo "Unknown option: -$OPTARG" >&2; usage ;;
+        * ) usage ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "$fastq" ]
 then
-    echo $0 'fastq genome mapper'
+    usage
 fi
+
+if [ -z "$GENOME" ]
+then
+    GENOME='/nfs/research2/bertone/common/genome/MM10/MM10'
+fi
+
+if [ -z "$OUT" ]
+then
+    OUT=$PWD"/bowtie/"
+else
+    if [[ ! $OUT =~ \/bowtie\/$ ]]
+    then
+        OUT="$(readlink -m ${OUT})/bowtie/"
+    fi
+fi
+# }}}
 
 # readlink converts relative to absolute path names
-fastq=$(readlink -m $1)
-genome=$(readlink -m $2)
+fastq=$(readlink -m ${fastq})
+GENOME=$(readlink -m ${GENOME})
 
-# Check if an argument was supplied for the mapper to use
-# -z checks if the argument is a NULL string and if so executed the body
-if [ -z "$3" ]
+mkdir -p ${OUT}
+
+describer=$(sed 's/\(.fastq.*\|.fq.*\|.txt\)$//' <<< $fastq | sed 's/.*\///')
+
+if [[ $fastq =~ ".gz" ]]
 then
-    # Set bowtie as the default mapper if an argument is not provided
-    mapper='bowtie'
-else
-    mapper=$3
+    gunzip -d $fastq
+    fastq=${fastq/.gz/}
+    #bowtie -m 1 -S -p 4 $GENOME $fastq > ${OUT}${describer}.sam
+    #gunzip $fastq
 fi
 
-out=$PWD'/'$mapper'/'
-
-if [ ! -d $out ]
-then
-     mkdir $out
-     echo -e "Creating $out"
-fi
-
-if [ $mapper == 'bowtie' ]
-then
-    describer=$(sed 's/\(.fastq.*\|.fq.*\|.txt\)$//' <<< $fastq | sed 's/.*\///')
-
-    if [[ $fastq =~ ".gz" ]]
-    then
-        gunzip -d $fastq
-        fastq=${fastq/.gz/}
-        bowtie -m 1 -S -p 4 $genome $fastq > ${out}${describer}.sam
-        gunzip $fastq
-    fi
-    # TODO: add more options if ever get PE reads for ChIP-seq or if I align RNA-Seq 
-    # with bowtie instead of gsnap (not gapped alignment)
-    # -p <n> launches parallel search threads => Always bsub it with -n 4
-    bowtie -m 1 -S -p 4 $genome $fastq > ${out}${describer}.sam
-    # Keeping a record of how the files in the directory where generated
-    echo -e `date +"%D%t%T"` "\t" "bowtie -m 1 -S $genome $fastq > ${out}${describer}.sam" >> ${out}FilesTree.log
+# TODO: add more options if ever get PE reads for ChIP-seq or if I align RNA-Seq 
+# with bowtie instead of gsnap (not gapped alignment)
+# -p <n> launches parallel search threads => Always bsub it with -n 4
+bowtie -m 1 -S -p 4 $GENOME $fastq > ${OUT}${describer}.sam
+# Keeping a record of how the files in the directory where generated
+echo -e `date +"%D%t%T"` "\t" "bowtie -m 1 -S $GENOME $fastq > ${OUT}${describer}.sam" >> ${OUT}FilesTree.log
 
 # TODO: add more peak callers if ever need to use a different one
-fi
 
 # SAM to BAM
-samtools view -bS ${out}${describer}.sam -o ${out}${describer}.uns.bam
-echo -e `date +"%D%t%T"` "\t" "samtools view -bS ${out}${describer}.sam -o ${out}${describer}.uns.bam" >> ${out}FilesTree.log
+samtools view -bS ${OUT}${describer}.sam -o ${OUT}${describer}.uns.bam
+#echo -e `date +"%D%t%T"` "\t" "samtools view -bS ${OUT}${describer}.sam -o ${OUT}${describer}.uns.bam" >> ${OUT}FilesTree.log
 
 # Sort BAM
-samtools sort ${out}${describer}.uns.bam ${out}${describer}.sort
-echo -e `date +"%D%t%T"` "\t" "samtools sort ${out}${describer}.uns.bam ${out}${describer}.sort" >> ${out}FilesTree.log
+samtools sort ${OUT}${describer}.uns.bam ${OUT}${describer}.sort
+#echo -e `date +"%D%t%T"` "\t" "samtools sort ${OUT}${describer}.uns.bam ${OUT}${describer}.sort" >> ${OUT}FilesTree.log
 
 # Sort BAM
-samtools index ${out}${describer}.sort.bam
-echo -e `date +"%D%t%T"` "\t" "samtools index ${out}${describer}.sort.bam" >> ${out}FilesTree.log
+samtools index ${OUT}${describer}.sort.bam
+#echo -e `date +"%D%t%T"` "\t" "samtools index ${OUT}${describer}.sort.bam" >> ${OUT}FilesTree.log
+
+# Regardless of whether the file was compressed to begin with, compress it
+gzip $fastq
 
 # Remove intermediate files
-#rm ${out}${describer}.sam
-#rm ${out}${describer}.uns.bam
+rm ${OUT}${describer}.sam
+rm ${OUT}${describer}.uns.bam
