de4c3fab85e4eb5170be38563d0d88b1ee3ba417
Maria Xenophontos <m.xenophontos.cy@gmail.com>
2016-01-22 15:41:06 +0000

    Added plot for acrual number of uniquely seq reads and formatted order of stacked barplot

--- plot-alignment-stats.R
+++ plot-alignment-stats.R
@@ -22,28 +22,68 @@ options(stringsAsFactors=F)
 library(ggplot2)
 library(reshape)
 library(dplyr)
+library(tidyr)
 
 for(f in args$file){
     bowtie = read.table(f, sep = "\t", header = TRUE)
-    print(head(bowtie))
     # dup_removed are all reads mapped_duplicate_free + unmapped reads (fail or more_mult)
     # so to get the uniquely mapped duplicate free read count one should subtract the failed alignments from 
     # dup_removed
-    bowtie = bowtie %>% mutate(unique = (dup_removed - fail - more_mult) * 100 / all,
-                               mapped = mapped * 100 / all,
+    bowtie = bowtie %>% mutate(unique = (dup_removed - fail - more_mult),
+                               mapped = mapped,
                                duplicated = (mapped - unique),
+                               fail = fail,
+                               more_mult = more_mult
+                               )
+    plot_data = melt((bowtie %>% select(filename, fail, more_mult, unique, duplicated, all)), id = 'filename')
+    plot_data = plot_data %>% tidyr::separate(filename, into = c("condition", "protein"), sep="-" ) %>%
+                mutate(protein = gsub(".stderr", "", protein),
+                       ID = paste(condition, protein, sep="-"))
+    plot_data[['value']] = as.numeric(plot_data[['value']])
+    plot_data[['ID']] = as.factor(plot_data[['ID']])
+    plot_data[['condition']] = as.factor(plot_data[['condition']])
+
+    cols = c('all' = "#B2506F",
+             'unique' = "#B05BB3",
+             'duplicated' = "#667EA0",
+             'more_mult' = "#B6653F",
+             'fail' = "#4A8F75")
+    plot_data[['variable']] = factor(plot_data[['variable']], levels = names(cols))
+
+    pdf(file.path(plot_path, 'bowtie-statistics.pdf'), page = 'a4')
+    for (cond in levels(plot_data$condition)){
+        q = ggplot(aes(x =ID, y = value / 1000000, fill = variable), data = subset(plot_data, condition == cond))
+        q = q + geom_bar(stat = "identity", width = .3, position = 'dodge')
+        q = q + theme_bw()
+        q = q + theme(axis.line = element_line(colour = "black"),
+                      panel.grid.major.x = element_blank(),
+                      panel.grid.major.y = element_line(),
+                      panel.grid.minor = element_blank(),
+                      panel.border = element_blank(),
+                      panel.background = element_blank(), 
+                      axis.text.x = element_text(angle = 25, hjust = 1))
+        q = q + scale_fill_manual(values = cols, breaks = names(cols), name = "Alignment types")
+        plot(q)
+    }
+
+    bowtie = bowtie %>% mutate(unique = unique * 100 / all,
+                               mapped = mapped * 100 / all,
+                               duplicated = duplicated * 100 /all,
                                fail = fail * 100 / all,
                                more_mult = more_mult * 100 / all
                                )
-    bowtie = bowtie %>% select(filename, fail, more_mult, unique, duplicated)
+    bowtie = bowtie %>% select(filename, unique, duplicated, more_mult, fail)
 
     plot_data = melt(bowtie, id = 'filename')
+    plot_data = plot_data %>% tidyr::separate(filename, into = c("condition", "protein"), sep="-" ) %>%
+                mutate(protein = gsub(".stderr", "", protein),
+                       ID = paste(condition, protein, sep="-"))
     plot_data[['value']] = as.numeric(plot_data[['value']])
-    
-    cols = c('fail' = "#4A8F75", 'more_mult' = "#B6653F", 'unique' = "#B05BB3", 'duplicated' = "#667EA0")
+    plot_data[['ID']] = as.factor(plot_data[['ID']])
+    plot_data[['condition']] = as.factor(plot_data[['condition']])
+    plot_data[['variable']] = factor(plot_data[['variable']], levels = names(cols))
 
-    pdf(file.path(plot_path, 'bowtie-statistics.pdf'))
-    q = ggplot(aes(x =filename, y = value, fill = variable), data = plot_data)
+    q = ggplot(aes(x =ID, y = value, fill = variable, order = variable), data = plot_data)
     q = q + geom_bar(stat = "identity", width = .3)
     q = q + theme_bw()
     q = q + coord_flip()
@@ -54,7 +94,7 @@ for(f in args$file){
                   panel.border = element_blank(),
                   panel.background = element_blank())
     q = q + scale_y_continuous(limits=c(0, 100), name = "Percentage of total reads")
-    q = q + scale_fill_manual(values = cols, name = "Alignment types")
+    q = q + scale_fill_manual(values = cols, breaks = names(cols), name = "Alignment types")
     plot(q)
 
     dev.off()
