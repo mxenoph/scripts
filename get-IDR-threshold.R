79a3d8e441ab7a1900b97194bd58d529c6392a7c
Maria Xenophontos <m.xenophontos.cy@gmail.com>
2016-02-15 11:57:16 +0000

    Simple R script for reading number of relaxed peaks in IDR experiment and defining thresholds

---
+++ get-IDR-threshold.R
@@ -0,0 +1,29 @@
+#!/usr/bin/env Rscript
+
+# Parsing command line arguments and create output subdirectories# {{{
+# Make library loading silent
+library = function (...) suppressMessages(base::library(...))
+library(argparse)
+library(tools)
+library(dplyr)
+
+parser =  ArgumentParser(description="Get IDR thresholds")
+parser$add_argument('-p', '--pre_IDR', metavar= "file", required='True', type= "character",
+                    help= "TSV with number of peaks before IDR generated with get-IDR-threshold PHONY in Makefile")
+
+args = parser$parse_args()
+#}}}
+
+target = file.path(dirname(args$pre_IDR), 'IDR-thresholds.tsv')
+
+df = read.delim(args$pre_IDR, head=F, stringsAsFactors=F)
+df = df %>% select(V2,V3) %>%
+    mutate(group = gsub("_[^-]*_peaks.*", "", V3)) %>%
+    filter(group != "total") %>%
+    group_by(group) %>% summarise(min = min(V2), max=max(V2)) %>% 
+    mutate(top = ifelse(min < 100000, 100000, 150000),
+           thr = ifelse(min < 100000, 0.05, 0.02)) %>%
+    tidyr::unite(output, group, thr, sep="_conservative_") %>%
+    mutate(output = basename(gsub("$", ".narrowPeak", output)))
+    
+write.table(df, target, col.names=T, row.names=F, quote=F, sep="\t")
