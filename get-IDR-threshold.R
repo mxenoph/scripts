83c6b52f617d7e3c85aaf43c8afae00028cf82dc
Maria Xenophontos <m.xenophontos.cy@gmail.com>
2016-02-16 15:24:30 +0000

    added parameter for returning thresholds for pooled consistency

--- get-IDR-threshold.R
+++ get-IDR-threshold.R
@@ -10,11 +10,22 @@ library(dplyr)
 parser =  ArgumentParser(description="Get IDR thresholds")
 parser$add_argument('-p', '--pre_IDR', metavar= "file", required='True', type= "character",
                     help= "TSV with number of peaks before IDR generated with get-IDR-threshold PHONY in Makefile")
+parser$add_argument('-o', '--output', metavar= "file", required='True', type= "character",
+                    help= "Output filename")
+parser$add_argument('-t', '--type', action ="store_true", default = FALSE, help = "Set to true for returning thresholds for pooled consistency. Default = FALSE")
 
 args = parser$parse_args()
 #}}}
 
-target = file.path(dirname(args$pre_IDR), 'IDR-thresholds.tsv')
+if(args$type){
+    print('Reporting thresholds for pooled consistency')
+    low = 0.005
+    high = 0.01
+} else {
+    print('Reporting thresholds for self consistency')
+    low = 0.02
+    high = 0.05
+}
 
 df = read.delim(args$pre_IDR, head=F, stringsAsFactors=F)
 df = df %>% select(V2,V3) %>%
@@ -22,8 +33,8 @@ df = df %>% select(V2,V3) %>%
     filter(group != "total") %>%
     group_by(group) %>% summarise(min = min(V2), max=max(V2)) %>% 
     mutate(top = ifelse(min < 100000, 100000, 150000),
-           thr = ifelse(min < 100000, 0.05, 0.02)) %>%
+           thr = ifelse(min < 100000, high, low)) %>%
     tidyr::unite(output, group, thr, sep="_conservative_") %>%
     mutate(output = basename(gsub("$", ".narrowPeak", output)))
     
-write.table(df, target, col.names=T, row.names=F, quote=F, sep="\t")
+write.table(df, args$output, col.names=T, row.names=F, quote=F, sep="\t")
